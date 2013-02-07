96c8199e8525c36cb4dfd39b03d4bb5ab5351d78
Stefan Heule <stefanheule@gmail.com>
2013-02-07 16:50:25 +0100

    Use publish-local on the build server.

--- project/build.scala
+++ project/build.scala
@@ -22,22 +22,24 @@ object SiliconBuild extends Build {
 
   /* Projects */
   
-	lazy val silicon = Project(
-    id = "silicon",
-    base = file("."),
-    settings = (
-         baseSettings
-      ++ Seq(
-            name := "Silicon",
-            traceLevel := 10,
-            maxErrors := 6,
-            libraryDependencies ++=
-                 dependencies.logging
-              :+ dependencies.scopt
-//              :+ dependencies.sil
-              /* :+ dependencies.scalaz :+ dependencies.scalatest */))
-  ).dependsOn(dependencies.sil, common)
-   .aggregate(common)
+	lazy val silicon = {
+    var p = Project(
+      id = "silicon",
+      base = file("."),
+      settings = (
+           baseSettings
+        ++ Seq(
+              name := "Silicon",
+              traceLevel := 10,
+              maxErrors := 6,
+              libraryDependencies ++= externalDep
+              ))
+    ).dependsOn(common)
+    for (dep <- internalDep) {
+      p = p.dependsOn(dep)
+    }
+    p.aggregate(common)
+  }
 
   lazy val common = Project(
     id = "common",
@@ -48,6 +50,14 @@ object SiliconBuild extends Build {
         name := "Silicon-Common",
         javacOptions ++= Seq("-source", "1.7", "-target", "1.7") /* ,
         libraryDependencies ++= Seq(dependencies.liftJson, dependencies.grizzled) */)))
+
+  // On the build-server, we cannot have all project in the same directory, and thus we use the publish-local mechanism for dependencies.
+  def isBuildServer = sys.env.contains("BUILD_TAG") // should only be defined on the build server
+  def internalDep = if (isBuildServer) Nil else Seq(dependencies.silDir)
+  def externalDep = {
+    dependencies.logging ++ Seq(dependencies.scopt) ++
+    (if (isBuildServer) Seq(dependencies.sil) else Nil)
+  }
 
   /* Dependencies */
 
@@ -60,7 +70,8 @@ object SiliconBuild extends Build {
     lazy val grizzled = "org.clapper" %% "grizzled-scala" % "1.0.13"
 //    lazy val sil = "semper" %% "sil" %  "0.1-SNAPSHOT"
 //    lazy val sil = uri("hg:https://mschwerhoff@bitbucket.org/semperproject/sil")
-    lazy val sil = RootProject(new java.io.File("../sil"))
+    lazy val sil = "semper" %% "sil" %  "0.1-SNAPSHOT"
+    lazy val silDir = RootProject(new java.io.File("../Sil"))
     // lazy val scalate = "org.fusesource.scalate" % "scalate-core" % "1.5.3"
     // lazy val scalamd = "org.fusesource.scalamd" % "scalamd" % "1.5"
     // lazy val scalateGenerator = "com.mojolly.scalate" % "xsbt-scalate-generator_2.9.1-1" % "0.11.2-0.1.6"
