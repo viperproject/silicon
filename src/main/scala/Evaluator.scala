fc0dfbddbbba6ec2693e93da61cd318ed226a234
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2014-04-25 08:45:17 +0200

    Filter redundant guard when joining different branches of local evaluations

--- src/main/scala/Evaluator.scala
+++ src/main/scala/Evaluator.scala
@@ -878,8 +878,7 @@ trait DefaultEvaluator[
 
     val (t1: Term, tAux: Set[Term]) =
       localResults.map {lr =>
-        val newGuards = lr.πGuards
-
+        val newGuards = lr.πGuards filterNot decider.π.contains
         val guard: Term = state.terms.utils.BigAnd(newGuards)
         val tAct: Term = Implies(guard, actualResultTransformer(lr.actualResult))
         val tAux: Term = Implies(guard, state.terms.utils.BigAnd(lr.auxiliaryTerms))
