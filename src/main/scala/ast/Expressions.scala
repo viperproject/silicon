bd0f4a35200141e9555c833c6d314e7bd814f6a1
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2014-07-13 17:11:03 +0200

    Resolved magic wand issue 13. The problem was that the bottom-up ghost operation erasure wasn't correctly implemented, which resulted in certain unfolding expressions not being recognised as impure unfolding ghost operations.

--- src/main/scala/ast/Expressions.scala
+++ src/main/scala/ast/Expressions.scala
@@ -20,15 +20,31 @@ object expressions {
     * @param e An expression to erase all ghost operations from.
     * @return The ghost-operations-free version of `e`.
     */
-  def eraseGhostOperations(e: ast.Expression): ast.Expression =
+  def eraseGhostOperations(e: ast.Expression): ast.Expression = {
     /* We use the post-transformer instead of the pre-transformer in order to
      * perform bottom-up transformation. With a top-down transformer we could
      * not simply replace ghost operations with their bodies, because these
      * can contain ghost operations themselves, to which the transformer
      * would not be applied (per se).
      */
+
+    /* False if we already encountered a ghost operation (that was not a pure
+     * unfolding). If so, then unfolding expressions are considered as impure
+     * ghost operations, and therefore replaced by their bodies.
+     * This is necessary to correctly handle expressions such as
+     *   acc(P(l)) --* unfolding P(l) in folding Q(l) in true
+     * where the outer unfolding must be erased although its body is pure
+     * by the time we reach the unfolding (since we transform bottom-up).
+     */
+    var keepUnfolding = true
+
     e.transform()(post = {
-      case u: ast.Unfolding if !u.isPure => u.body
-      case gop: ast.GhostOperation if !gop.isInstanceOf[ast.Unfolding] => gop.body
+      case u: ast.Unfolding if !u.isPure || !keepUnfolding =>
+        u.body
+
+      case gop: ast.GhostOperation if !gop.isInstanceOf[ast.Unfolding] =>
+        keepUnfolding = false
+        gop.body
     })
+  }
 }
