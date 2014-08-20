2fb576bb42b0b2857328d6f8e800412b50c27766
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2014-08-20 11:01:30 +0200

    Added missing translation of set and multiset expressions

--- src/main/scala/theories/ExpressionTranslator.scala
+++ src/main/scala/theories/ExpressionTranslator.scala
@@ -2,8 +2,8 @@ package viper
 package silicon
 package theories
 
-import state.{terms, SymbolConvert}
-import state.terms.Term
+import state.SymbolConvert
+import state.terms._
 
 trait ExpressionTranslator {
   val symbolConverter: SymbolConvert
@@ -22,89 +22,137 @@ trait ExpressionTranslator {
    *
    * TODO: Can't we do without toSort? Or at least with a less type-specific one?
    */
-  protected def translate(toSort: (ast.Type, Map[ast.TypeVar, ast.Type]) => terms.Sort)
+  protected def translate(toSort: (ast.Type, Map[ast.TypeVar, ast.Type]) => Sort)
                          (exp: ast.Expression)
                          : Term = {
 
     val f = translate(toSort) _
 
+    def translateAnySetUnExp(exp: silver.ast.AnySetUnExp,
+                             setTerm: Term => Term,
+                             multisetTerm: Term => Term) =
+
+      exp.typ match {
+        case _: ast.types.Set => setTerm(f(exp.exp))
+        case _: ast.types.Multiset => multisetTerm(f(exp.exp))
+        case _ => sys.error("Expected a (multi)set-typed expression but found %s (%s) of sort %s"
+                            .format(exp, exp.getClass.getName, exp.typ))
+      }
+
+    def translateAnySetBinExp(exp: silver.ast.AnySetBinExp,
+                              setTerm: (Term, Term) => Term,
+                              multisetTerm: (Term, Term) => Term,
+                              anysetTypedExp: ast.Expression = exp) =
+
+      anysetTypedExp.typ match {
+        case _: ast.types.Set => setTerm(f(exp.left), f(exp.right))
+        case _: ast.types.Multiset => multisetTerm(f(exp.left), f(exp.right))
+        case _ => sys.error("Expected a (multi)set-typed expression but found %s (%s) of sort %s"
+                            .format(anysetTypedExp, anysetTypedExp.getClass.getName, anysetTypedExp.typ))
+      }
+
+
     exp match {
       /* TODO: Translate triggers */
       case q @ ast.Quantified(qvars, body) =>
         val (quantifier, triggers) = q match {
-          case fa: ast.Forall => (terms.Forall, fa.autoTrigger.triggers)
-          case _: ast.Exists => (terms.Exists, Nil)
+          case fa: ast.Forall => (Forall, fa.autoTrigger.triggers)
+          case _: ast.Exists => (Exists, Nil)
         }
-        terms.Quantification(quantifier,
-                             qvars map (v => terms.Var(v.name, toSort(v.typ, Map()))),
-                             f(body),
-                             triggers map (tr => terms.Trigger(tr.exps map f)))
 
-      case ast.True() => terms.True()
-      case ast.False() => terms.False()
-      case ast.Not(e0) => terms.Not(f(e0))
-      case ast.And(e0, e1) => terms.And(f(e0), f(e1))
-      case ast.Or(e0, e1) => terms.Or(f(e0), f(e1))
-      case ast.Implies(e0, e1) => terms.Implies(f(e0), f(e1))
-      case ast.Ite(e0, e1, e2) => terms.Ite(f(e0), f(e1), f(e2))
+        Quantification(quantifier,
+                       qvars map (v => Var(v.name, toSort(v.typ, Map()))),
+                       f(body),
+                       triggers map (tr => Trigger(tr.exps map f)))
 
-      case ast.Equals(e0, e1) => terms.Eq(f(e0), f(e1))
-      case ast.Unequals(e0, e1) => terms.Not(terms.Eq(f(e0), f(e1)))
+      case ast.True() => True()
+      case ast.False() => False()
+      case ast.Not(e0) => Not(f(e0))
+      case ast.And(e0, e1) => And(f(e0), f(e1))
+      case ast.Or(e0, e1) => Or(f(e0), f(e1))
+      case ast.Implies(e0, e1) => Implies(f(e0), f(e1))
+      case ast.Ite(e0, e1, e2) => Ite(f(e0), f(e1), f(e2))
 
-      case ast.IntegerLiteral(n) => terms.IntLiteral(n)
-      case ast.IntPlus(e0, e1) => terms.Plus(f(e0), f(e1))
-      case ast.IntMinus(e0, e1) => terms.Minus(f(e0), f(e1))
-      case ast.IntTimes(e0, e1) => terms.Times(f(e0), f(e1))
-      case ast.IntDiv(e0, e1) => terms.Div(f(e0), f(e1))
-      case ast.IntMod(e0, e1) => terms.Mod(f(e0), f(e1))
-      case ast.IntNeg(e0) => terms.Minus(terms.IntLiteral(0), f(e0))
+      case ast.Equals(e0, e1) => Eq(f(e0), f(e1))
+      case ast.Unequals(e0, e1) => Not(Eq(f(e0), f(e1)))
 
-      case ast.IntGE(e0, e1) => terms.AtLeast(f(e0), f(e1))
-      case ast.IntGT(e0, e1) => terms.Greater(f(e0), f(e1))
-      case ast.IntLE(e0, e1) => terms.AtMost(f(e0), f(e1))
-      case ast.IntLT(e0, e1) => terms.Less(f(e0), f(e1))
+      case ast.IntegerLiteral(n) => IntLiteral(n)
+      case ast.IntPlus(e0, e1) => Plus(f(e0), f(e1))
+      case ast.IntMinus(e0, e1) => Minus(f(e0), f(e1))
+      case ast.IntTimes(e0, e1) => Times(f(e0), f(e1))
+      case ast.IntDiv(e0, e1) => Div(f(e0), f(e1))
+      case ast.IntMod(e0, e1) => Mod(f(e0), f(e1))
+      case ast.IntNeg(e0) => Minus(IntLiteral(0), f(e0))
 
-      case ast.NullLiteral() => terms.Null()
+      case ast.IntGE(e0, e1) => AtLeast(f(e0), f(e1))
+      case ast.IntGT(e0, e1) => Greater(f(e0), f(e1))
+      case ast.IntLE(e0, e1) => AtMost(f(e0), f(e1))
+      case ast.IntLT(e0, e1) => Less(f(e0), f(e1))
 
-      case v: ast.Variable => terms.Var(v.name, toSort(v.typ, Map()))
+      case ast.NullLiteral() => Null()
+
+      case v: ast.Variable => Var(v.name, toSort(v.typ, Map()))
 
       case ast.DomainFuncApp(funcName, args, typeVarMap) =>
         val tArgs = args map f
         val inSorts = tArgs map (_.sort)
         val outSort = toSort(exp.typ, toMap(typeVarMap))
         val id = symbolConverter.toSortSpecificId(funcName, inSorts :+ outSort)
-        val df = terms.Function(id, inSorts, outSort)
-        terms.DomainFApp(df, tArgs)
+        val df = Function(id, inSorts, outSort)
+        DomainFApp(df, tArgs)
 
-      case _: ast.FullPerm => terms.FullPerm()
-      case _: ast.NoPerm => terms.NoPerm()
-      case ast.FractionalPerm(e0, e1) => terms.FractionPerm(terms.TermPerm(f(e0)), terms.TermPerm(f(e1)))
+      /* Permissions */
 
-      case ast.PermPlus(e0, e1) => terms.PermPlus(terms.TermPerm(f(e0)), terms.TermPerm(f(e1)))
-      case ast.PermMinus(e0, e1) => terms.PermMinus(terms.TermPerm(f(e0)), terms.TermPerm(f(e1)))
-      case ast.PermTimes(e0, e1) => terms.PermTimes(terms.TermPerm(f(e0)), terms.TermPerm(f(e1)))
-      case ast.IntPermTimes(e0, e1) => terms.IntPermTimes(f(e0), terms.TermPerm(f(e1)))
-      case ast.PermLE(e0, e1) => terms.AtMost(f(e0), f(e1))
-      case ast.PermLT(e0, e1) => terms.Less(f(e0), f(e1))
-      case ast.PermGE(e0, e1) => terms.AtLeast(f(e0), f(e1))
-      case ast.PermGT(e0, e1) => terms.Greater(f(e0), f(e1))
+      case _: ast.FullPerm => FullPerm()
+      case _: ast.NoPerm => NoPerm()
+      case ast.FractionalPerm(e0, e1) => FractionPerm(TermPerm(f(e0)), TermPerm(f(e1)))
 
-      case silver.ast.SeqAppend(e0, e1) => terms.SeqAppend(f(e0), f(e1))
-      case silver.ast.SeqContains(e0, e1) => terms.SeqIn(f(e1), f(e0))
-      case silver.ast.SeqDrop(e0, e1) => terms.SeqDrop(f(e0), f(e1))
-      case silver.ast.SeqIndex(e0, e1) => terms.SeqAt(f(e0), f(e1))
-      case silver.ast.SeqLength(e) => terms.SeqLength(f(e))
-      case silver.ast.SeqTake(e0, e1) => terms.SeqTake(f(e0), f(e1))
-      case silver.ast.EmptySeq(typ) => terms.SeqNil(toSort(typ, Map()))
-      case silver.ast.RangeSeq(e0, e1) => terms.SeqRanged(f(e0), f(e1))
-      case silver.ast.SeqUpdate(e0, e1, e2) => terms.SeqUpdate(f(e0), f(e1), f(e2))
+      case ast.PermPlus(e0, e1) => PermPlus(TermPerm(f(e0)), TermPerm(f(e1)))
+      case ast.PermMinus(e0, e1) => PermMinus(TermPerm(f(e0)), TermPerm(f(e1)))
+      case ast.PermTimes(e0, e1) => PermTimes(TermPerm(f(e0)), TermPerm(f(e1)))
+      case ast.IntPermTimes(e0, e1) => IntPermTimes(f(e0), TermPerm(f(e1)))
+      case ast.PermLE(e0, e1) => AtMost(f(e0), f(e1))
+      case ast.PermLT(e0, e1) => Less(f(e0), f(e1))
+      case ast.PermGE(e0, e1) => AtLeast(f(e0), f(e1))
+      case ast.PermGT(e0, e1) => Greater(f(e0), f(e1))
+
+      /* Sequences */
+
+      case silver.ast.SeqAppend(e0, e1) => SeqAppend(f(e0), f(e1))
+      case silver.ast.SeqContains(e0, e1) => SeqIn(f(e1), f(e0))
+      case silver.ast.SeqDrop(e0, e1) => SeqDrop(f(e0), f(e1))
+      case silver.ast.SeqIndex(e0, e1) => SeqAt(f(e0), f(e1))
+      case silver.ast.SeqLength(e) => SeqLength(f(e))
+      case silver.ast.SeqTake(e0, e1) => SeqTake(f(e0), f(e1))
+      case silver.ast.EmptySeq(typ) => SeqNil(toSort(typ, Map()))
+      case silver.ast.RangeSeq(e0, e1) => SeqRanged(f(e0), f(e1))
+      case silver.ast.SeqUpdate(e0, e1, e2) => SeqUpdate(f(e0), f(e1), f(e2))
 
       case silver.ast.ExplicitSeq(es) =>
-        es.tail.foldLeft[terms.SeqTerm](terms.SeqSingleton(f(es.head)))((tSeq, e) =>
-          terms.SeqAppend(terms.SeqSingleton(f(e)), tSeq))
+        es.tail.foldLeft[SeqTerm](SeqSingleton(f(es.head)))((tSeq, e) =>
+          SeqAppend(SeqSingleton(f(e)), tSeq))
 
-      case _: silver.ast.MultisetExp | _: silver.ast.EmptySet | _: silver.ast.ExplicitSet =>
-        sys.error(s"Found unexpected expression $exp (${exp.getClass.getName}})")
+      /* Sets and multisets */
+
+      case silver.ast.EmptySet(typ) => EmptySet(toSort(typ, Map()))
+      case silver.ast.EmptyMultiset(typ) => EmptyMultiset(toSort(typ, Map()))
+
+      case silver.ast.ExplicitSet(es) =>
+        es.tail.foldLeft[SetTerm](SingletonSet(f(es.head)))((tSet, e) =>
+          SetAdd(tSet, f(e)))
+
+      case silver.ast.ExplicitMultiset(es) =>
+        es.tail.foldLeft[MultisetTerm](SingletonMultiset(f(es.head)))((tMultiset, e) =>
+          MultisetAdd(tMultiset, f(e)))
+
+      case as: silver.ast.AnySetUnion => translateAnySetBinExp(as, SetUnion, MultisetUnion)
+      case as: silver.ast.AnySetIntersection => translateAnySetBinExp(as, SetIntersection, MultisetIntersection)
+      case as: silver.ast.AnySetSubset => translateAnySetBinExp(as, SetSubset, MultisetSubset)
+      case as: silver.ast.AnySetMinus => translateAnySetBinExp(as, SetDifference, MultisetDifference)
+      case as: silver.ast.AnySetContains => translateAnySetBinExp(as, SetIn, MultisetIn, as.right)
+      case as: silver.ast.AnySetCardinality => translateAnySetUnExp(as, SetCardinality, MultisetCardinality)
+
+      /* Unsupported (because unexpected) expressions */
 
       case   _: ast.LocationAccess | _: ast.AccessPredicate | _: ast.Old | _: ast.FractionalPerm
              | _: ast.ResultLiteral | _: ast.Unfolding | _: ast.InhaleExhale | _: ast.PredicateAccess
