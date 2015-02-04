4628d6ae4e71e195140615b06ffd2111bb17fdf9
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2015-02-04 09:53:46 +0100

    Added missing, wand-related match cases

--- src/main/scala/supporters/ExpressionTranslator.scala
+++ src/main/scala/supporters/ExpressionTranslator.scala
@@ -161,7 +161,8 @@ trait ExpressionTranslator {
 
       case   _: ast.LocationAccess | _: ast.AccessPredicate | _: ast.Old | _: ast.FractionalPerm
              | _: ast.Result | _: ast.Unfolding | _: ast.InhaleExhaleExp | _: ast.PredicateAccess
-             | _: ast.FuncApp | _: ast.CurrentPerm | _: ast.EpsilonPerm  | _: ast.WildcardPerm | _: ast.EpsilonPerm =>
+             | _: ast.FuncApp | _: ast.CurrentPerm | _: ast.EpsilonPerm  | _: ast.WildcardPerm | _: ast.EpsilonPerm
+             | _: ast.ApplyOld | _: ast.Applying | _: ast.Folding | _: ast.MagicWand | _: ast.Packaging =>
 
         sys.error(s"Found unexpected expression $exp (${exp.getClass.getName}})")
     }
