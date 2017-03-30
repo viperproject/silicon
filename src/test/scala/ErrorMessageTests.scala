b8a056cd87903a9abf0650abb1a297e68e5c1612
Simon Fritsche <simon.fritsche+bitbucket@gmail.com>
2017-03-30 17:55:22 +0200

    Added MPL header

--- src/test/scala/ErrorMessageTests.scala
+++ src/test/scala/ErrorMessageTests.scala
@@ -1,6 +1,8 @@
-/**
-  * Created by simonfri on 11.01.2017.
-  */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package viper.silicon.tests
 
 import java.nio.file.{Path, Paths}
