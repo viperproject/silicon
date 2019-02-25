b846bc36b3a5741618a2eee05b1e46a1373f3588
Fábio Pakk Selmi-Dei <fabio.pakk@inf.ethz.ch>
2019-02-25 15:40:09 +0100

    Fixed wrong license.

--- utils/scripts/sum.scala
+++ utils/scripts/sum.scala
@@ -1,8 +1,5 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-//
-// Copyright (c) 2011-2019 ETH Zurich.
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
 
 import java.io.{FileOutputStream, OutputStreamWriter, BufferedWriter}
 import scala.collection.mutable
