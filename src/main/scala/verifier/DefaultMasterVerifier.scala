8d4caff464f1a42e8d87f8548f50d7c181f23093
Nicolas Trüssel <nicolast@student.ethz.ch>
2019-07-04 10:12:58 +0200

    Selectively Disable MoreCompleteExhale during Function and Predicate verification

--- src/main/scala/verifier/DefaultMasterVerifier.scala
+++ src/main/scala/verifier/DefaultMasterVerifier.scala
@@ -255,7 +255,8 @@ class DefaultMasterVerifier(config: Config, override val reporter: Reporter)
           qpMagicWands = quantifiedMagicWands,
           applyHeuristics = applyHeuristics,
           predicateSnapMap = predSnapGenerator.snapMap,
-          predicateFormalVarMap = predSnapGenerator.formalVarMap)
+          predicateFormalVarMap = predSnapGenerator.formalVarMap,
+          hack = false)
   }
 
   private def createInitialState(cfg: SilverCfg, program: ast.Program): State = {
