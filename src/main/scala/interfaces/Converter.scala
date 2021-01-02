b3fab56f287f8eaab0bbb6dda687a379d5cabd7f
cedric <hegglinc@student.ethz.ch>
2021-01-02 17:27:58 +0100

    Map local variables to extracted values on heap into ExtractedEntries

--- src/main/scala/interfaces/Converter.scala
+++ src/main/scala/interfaces/Converter.scala
@@ -6,31 +6,60 @@ import viper.silver.ast
 import viper.silicon.interfaces.state.Chunk
 import viper.silicon.resources.{FieldID, PredicateID}
 import viper.silicon.state.{Store, State, BasicChunk, Identifier}
-import viper.silicon.state.terms.{sorts, Sort, Term, Unit, IntLiteral, Null, Var, App, Combine,
+import viper.silicon.state.terms.{sorts, Sort, Term, Unit, IntLiteral, BooleanLiteral, Null, Var, App, Combine,
                                   First, Second, SortWrapper, PredicateLookup, toSnapTree, Rational, PermLiteral}
 
 
 /* Some new classes to describe a more informative model */
 
-sealed trait ExtModelEntry
-case class LitIntEntry(value: BigInt) extends ExtModelEntry 
-case class LitBoolEntry(value: Boolean) extends ExtModelEntry
-case class RefEntry(fields: Map[String, ExtModelEntry]) extends ExtModelEntry
-case class VarEntry(name: String)
-case class NullRefEntry() extends ExtModelEntry
-case class ExtMapEntry(options: Map[String, ExtModelEntry]) extends ExtModelEntry
-case class OtherEntry(value: String) extends ExtModelEntry
+sealed trait ExtractedModelEntry
+case class LitIntEntry(value: BigInt) extends ExtractedModelEntry {
+    override def toString = value.toString
+}
+case class LitBoolEntry(value: Boolean) extends ExtractedModelEntry {
+    override def toString = value.toString
+}
+case class RefEntry(fields: Map[String, ExtractedModelEntry]) extends ExtractedModelEntry {
+    override def toString : String = {
+        var buf = "Ref : { \n"
+        for ((name, entry) <- fields) {
+            buf += "\t" + name + " <- " + entry.toString.split("\n").mkString("\n\t") + "\n"
+        }
+        buf += "}"
+        return buf
+    }
+}
+case class VarEntry(name: String) {
+    override def toString = name
+}
+case class NullRefEntry() extends ExtractedModelEntry {
+    override def toString = "Null()"
+}
+case class OtherEntry(value: String) extends ExtractedModelEntry {
+    override def toString = value + "(unhandled type)"
+}
 
-case class ExtModel(entries: Map[String, ExtModelEntry])
+case class ExtractedModel(entries: Map[String, ExtractedModelEntry]) {
+    override def toString : String = {
+        var buf = ""
+        for ((name, entry) <- entries) {
+            buf += name + " <- " + entry.toString + "\n"
+        }
+        return buf
+    }
+}
 
 sealed trait HeapEntry
 case class PredHeapEntry(name: String, args: Array[Term]) extends HeapEntry {
     override def toString = name + "(" + args.mkString(", ") +")"
 }
-case class FieldHeapEntry(recv: Term, field: String, perm: Rational) extends HeapEntry {
-    override def toString = recv.toString + "." + field
+case class FieldHeapEntry(recv: String, field: String, perm: Rational, typ: Option[ast.Type]) extends HeapEntry {
+    override def toString = recv + "." + field
 }
 
+case class ExtractedHeap(entries: Map[HeapEntry, String]) {
+    override def toString = entries.map(x => x._1.toString + " <- " + x._2).mkString("\n")
+}
 
 
 /* basically a 1 to 1 copy of nagini code */
@@ -70,12 +99,24 @@ object Converter{
             case _ => s.toString
         }
     }
+
+    def sortToType(s:Sort) : Option[ast.Type] = {
+        s match {
+            case sorts.Ref => return Some(ast.Ref)
+            case sorts.Int => return Some(ast.Int)
+            case sorts.Bool => return Some(ast.Bool)
+            case sorts.Perm => return Some(ast.Perm)
+            case _ => println("This type of Heap Field can not be handled : " + s.toString)
+                      return None
+        }
+    }
     
 
     def evaluateTerm(term:Term, model: Model) : String = {
         term match {
             case Unit => return "$Snap.unit"
             case IntLiteral(n) => return term.toString
+            case t:BooleanLiteral => return t.value.toString
             case Null() => return model.entries("$Ref.null").toString
             case Var(id, sort) => {
                 val key = term.toString
@@ -160,52 +201,87 @@ object Converter{
 
     def extractField(chunk:BasicChunk, model: Model) : (HeapEntry, String) =  {
         val fieldname = chunk.id.name //String
-        var recv : Term = chunk.args.head //Term
-        recv match {
-            case Var(id, sort) => //no evaluation necessary
-            case t: Term => val recvString = evaluateTerm(recv, model)
-                            recv = Var(Identifier.apply(recvString), sorts.Ref)
-                            //has to be of sort Ref if there are fields to it
-        }
+        var recvString = evaluateTerm(chunk.args.head, model) //Term
+        
         val perm : Rational = chunk.perm match {
             case p : PermLiteral => p.literal
             case _ => println("Converter: permission field of chunk is not PermLiteral but " + chunk.perm.toString)
                       Rational.zero
         }
         val value = evaluateTerm(chunk.snap, model) //String
-        val entry = FieldHeapEntry(recv, fieldname, perm)
+        val typ : Option[ast.Type] = sortToType(chunk.snap.sort)
+        val entry = FieldHeapEntry(recvString, fieldname, perm, typ)
         return (entry, value)
     }
 
     def extractPredicate(chunk: BasicChunk, model: Model) : HeapEntry = {
         //this might be too simple for some cases but for prusti to tell if some 
         //variable is part of a class it should be good enough
-        //not really sure if the snap value is needed
+        //not really sure if the snap value should be added, seems to be same as one of 
+        //the args in most cases.
         val entry = PredHeapEntry(chunk.id.toString, chunk.args.toArray) 
         return entry
-    }    
-    /*
-    def extractRecursive(term: Term) : ExtModelEntry {
-        term match {
-            case Var(id, sort) => 
-            case  => 
-        }
-    }
-    
-    def extModelFromHeap(heap: SimpleHeap, store:Store, model: Model) {
-        for ((variable: ast.AbstractLocalVar, term: Term) <- store.values) {
-            val name = variable match {
-                ast.LocalVar(n, typ) => n
-                ast.Result(typ) => "Result()"
-            }
-            val sort = term match {
-                case Var(name, sort) => sort
-                case _ => println("Converter.extModelFromHeap(), usually the store only contains 
-                    only variables, no concrete values, this case has yet to be handled")
-            }
+    } 
 
+    def mapLocalVar(expectedType: ast.Type, termEval: String, heap: ExtractedHeap, model: Model) : ExtractedModelEntry = {
+        expectedType match {
+            case ast.Int => //value is possibly stored directly in store, but if it's stored in the model
+                            //we will have to parse it so we evaluate it anyways and parse it..
+                val value = BigInt(termEval) //TODO: catch exception if parsing fails. not sure if this could occur
+                return LitIntEntry(value)
+
+            case ast.Bool => 
+                val b : Boolean = termEval.toLowerCase() match {
+                    case "true" => true
+                    case "false" => false
+                    case x:String => println("error mapping counterexample : " + x + " not a boolean")
+                              return OtherEntry(termEval)
+                }
+                return LitBoolEntry(b)
+            case ast.Ref =>
+                println("trying to map Ref: " + termEval)
+                var map : Map[String, ExtractedModelEntry] = Map()
+                for ((entry: HeapEntry, value: String) <- heap) {
+                    entry match {
+                        case FieldHeapEntry(recv, field, perm, typ) =>
+                            if (recv == termEval) {
+                                if (typ.isDefined) {
+                                    val recEntry = mapLocalVar(typ.get, value, heap, model)
+                                    map += (field -> recEntry)
+                                } else {
+                                    map += (field -> OtherEntry(value))
+                                }
+                            }
+                        case _ => 
+                    }
+                }
+                return RefEntry(map)
+            case _ => return OtherEntry(termEval)
         }
-    */
+        
+    }
+
+
+    def mapHeapToStore(store: Store, heap: ExtractedHeap, model: Model) : ExtractedModel = {
+        var map : Map[String, ExtractedModelEntry] = Map()
+        for ((variable: ast.AbstractLocalVar, term: Term) <- store.values) {
+            var localtype : ast.Type = ast.Int
+            val name = variable match {
+                case ast.LocalVar(n, typ) => 
+                    localtype = typ
+                    n
+                case ast.Result(typ) => 
+                    localtype = typ
+                    "Result()"
+            }
+            val termEval = evaluateTerm(term, model)
+            val entry = mapLocalVar(localtype, termEval, heap, model)
+            map += (name -> entry)
+        }
+        val extrModel = ExtractedModel(map)
+        println(extrModel.toString)
+        return extrModel
+    }
 
     // might not be possible to display these new models with the old Model structure,
     // since those are unordered and we would want entries grouped.
@@ -226,6 +302,11 @@ object Converter{
             heapToModel(heap, lbl)
         }
     }
+
+    def extractedModeltoModel(extModel: ExtractedModel) : Model = {
+        val map = extModel.entries.map(x => (x._1 -> SingleEntry(x._2.toString)))
+        return Model(map)
+    }
 }   
     
 
@@ -235,8 +316,11 @@ case class Converter(model: Model, store: Store, heap: Iterable[Chunk], oldHeaps
     val extractedHeaps : Map[String, Converter.ExtractedHeap] = oldHeaps.map(x => x._1 -> Converter.extractHeap(x._2.values, model))
     val heapModel : Map[String, ModelEntry] = {
         Converter.outputOldHeaps(extractedHeaps)
+        Converter.mapHeapToStore(store, extractedHeap, model)
         Converter.heapToModel(extractedHeap, "")
     }
+    val extModel = Converter.extractedModeltoModel(Converter.mapHeapToStore(store, extractedHeap, model))
+    
 }
 
 
