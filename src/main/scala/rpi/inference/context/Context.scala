86bab2ec9a7c793ebd10c420de50b0ab2b1db38d
dohrau <jerome.dohrau@inf.ethz.ch>
2021-01-28 20:00:35 +0100

    Generate annotation for temporary variables.

--- src/main/scala/rpi/inference/context/Context.scala
+++ src/main/scala/rpi/inference/context/Context.scala
@@ -1,7 +1,7 @@
 package rpi.inference.context
 
 import rpi.builder.ProgramBuilder
-import rpi.{Configuration, Names, inference}
+import rpi.{Configuration, Names}
 import rpi.inference._
 import rpi.inference.annotation.Annotation
 import rpi.util.Namespace
@@ -259,8 +259,8 @@ private class CheckBuilder(program: ast.Program) extends ProgramBuilder {
         val processed = original.copy(thn = processedThen, els = processedElse)(original.pos, original.info, original.errT)
         addStatement(processed)
       case original@ast.While(condition, _, _) =>
+        // loop instrumentation
         addInstrumented {
-          // loop instrumentation
           val check = createCheck(original, declarations)
           addExhale(check.invariant)
           addCut(original, check)
@@ -269,28 +269,32 @@ private class CheckBuilder(program: ast.Program) extends ProgramBuilder {
         }
       case original@ast.MethodCall(name, arguments, _) =>
         if (Names.isAnnotation(name)) {
-          val annotation = inference.annotation.Annotation(name, arguments.head)
+          val annotation = Annotation(name, arguments.head)
           annotations.append(annotation)
         } else {
+          // make sure all arguments are variables
+          val variables = arguments.map {
+            case variable: ast.LocalVar =>
+              variable
+            case field: ast.FieldAccess =>
+              // create auxiliary variable
+              val name = namespace.uniqueIdentifier("t", Some(0))
+              val variable = makeVariable(name, field.typ)
+              // add annotation
+              val annotation = Annotation(Names.downAnnotation, variable)
+              annotations.append(annotation)
+              // assign to variable and return variable
+              addAssign(variable, field)
+              variable
+            case argument =>
+              sys.error(s"Unexpected argument: $argument")
+          }
+          val adapted = original.copy(args = variables)(original.pos, original.info, original.errT)
+          // method call instrumentation
           addInstrumented {
-            // make sure all arguments are variables
-            val variables = arguments.map {
-              case variable: ast.LocalVar =>
-                variable
-              case field: ast.FieldAccess =>
-                // create auxiliary variable
-                val name = namespace.uniqueIdentifier("t", Some(0))
-                val variable = makeVariable(name, field.typ)
-                // assign to variable and return variable
-                addAssign(variable, field)
-                variable
-              case argument =>
-                sys.error(s"Unexpected argument: $argument")
-            }
-            // method call instrumentation
             val check = methods(name)
             addExhale(check.precondition(variables))
-            addCut(original, check)
+            addCut(adapted, check)
             addInhale(check.postcondition(variables))
           }
         }
