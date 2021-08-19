d8f036eeb57f1e2fd8b3a573a489ee5fa52e960e
Josua Stuck <jstuck@ethz.ch>
2021-08-19 12:03:07 +0200

    increment Failure count on error creation

--- src/main/scala/rules/SymbolicExecutionRules.scala
+++ src/main/scala/rules/SymbolicExecutionRules.scala
@@ -6,7 +6,7 @@
 
 package viper.silicon.rules
 
-import viper.silicon.interfaces.{Failure, SiliconNativeCounterexample, SiliconRawCounterexample, SiliconVariableCounterexample, SiliconMappedCounterexample}
+import viper.silicon.interfaces.{Failure, SilFailureContext, SiliconMappedCounterexample, SiliconNativeCounterexample, SiliconRawCounterexample, SiliconVariableCounterexample}
 import viper.silicon.state.State
 import viper.silicon.verifier.Verifier
 import viper.silver.verifier.errors.ErrorWrapperWithExampleTransformer
@@ -14,6 +14,7 @@ import viper.silver.verifier.{Counterexample, CounterexampleTransformer, Model, 
 
 trait SymbolicExecutionRules {
   protected def createFailure(ve: VerificationError, v: Verifier, s: State, generateNewModel: Boolean = false): Failure = {
+    if (s.retryLevel == 0) Verifier.errorsReportedSoFar.incrementAndGet()
     var ceTrafo: Option[CounterexampleTransformer] = None
     val res = ve match {
       case ErrorWrapperWithExampleTransformer(wrapped, trafo) =>
