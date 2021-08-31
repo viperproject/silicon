992d316157bc0c7832fa1918c97c27ddd40b55ae
Josua Stuck <jstuck@ethz.ch>
2021-08-31 14:26:55 +0200

    typo

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -454,7 +454,7 @@ class Config(args: Seq[String]) extends SilFrontendConfig(args, "Silicon") {
   )
 
   val numberOfErrorsToReport: ScallopOption[Int] = opt[Int]("numberOfErrorsToReport",
-    descr = "Number of errors per Member before the verifier stops. If this number is set to 0, all errors are reported.",
+    descr = "Number of errors per member before the verifier stops. If this number is set to 0, all errors are reported.",
     default = Some(1),
     noshort = true
   )
