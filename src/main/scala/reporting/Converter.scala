954335f0f985054374e349cd2b9d74235c505a3c
Marco Eilers <eilers.marco@googlemail.com>
2022-12-20 16:49:36 +0100

    Adapting to Silver changes

--- src/main/scala/reporting/Converter.scala
+++ src/main/scala/reporting/Converter.scala
@@ -675,8 +675,8 @@ object Converter {
 
     val smtfunc = func match {
       case t: ast.Function => symbolConverter.toFunction(t).id
+      case t@ast.BackendFunc(_, _, _, _) => symbolConverter.toFunction(t, program).id
       case t: ast.DomainFunc => symbolConverter.toFunction(t, argSort :+ resSort, program).id
-      case t: ast.BackendFunc => symbolConverter.toFunction(t).id
     }
     val kek = smtfunc.toString
       .replace("[", "<")
