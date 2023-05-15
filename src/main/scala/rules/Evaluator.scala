d7db3717ca36ca1420f4cc97236df5a7bace2970
João Pereira <joaopereira.19@gmail.com>
2023-05-15 21:03:08 +0200

    Fix silver's issue #688 (#718)
    * Fix Silver's issue #688
    
    * use the correct verifier instance
    
    * use func. recorder

--- src/main/scala/rules/Evaluator.scala
+++ src/main/scala/rules/Evaluator.scala
@@ -290,8 +290,12 @@ object evaluator extends EvaluationRules {
             evalInOldState(s, lbl, e0, pve, v)(Q)}
 
       case ast.Let(x, e0, e1) =>
-        eval(s, e0, pve, v)((s1, t0, v1) =>
-          eval(s1.copy(g = s1.g + (x.localVar, t0)), e1, pve, v1)(Q))
+        eval(s, e0, pve, v)((s1, t0, v1) => {
+          val t = v1.decider.fresh(v1.symbolConverter.toSort(x.typ))
+          v1.decider.assume(t === t0)
+          val newFuncRec = s1.functionRecorder.recordFreshSnapshot(t)
+          eval(s1.copy(g = s1.g + (x.localVar, t), functionRecorder = newFuncRec), e1, pve, v1)(Q)
+        })
 
       /* Strict evaluation of AND */
       case ast.And(e0, e1) if Verifier.config.disableShortCircuitingEvaluations() =>
