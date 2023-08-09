f57fb1361a5811a992fe7b482f1e384cbce1113d
Dspil <dennisspiliopoylos@gmail.com>
2023-08-09 03:18:14 +0200

    stuff

--- src/main/scala/logger/benchmarker/SymbExLogBenchmarker.scala
+++ src/main/scala/logger/benchmarker/SymbExLogBenchmarker.scala
@@ -1,16 +1,15 @@
 package viper.silicon.logger.benchmarker
 
+import viper.silicon.logger.MemberSymbExLog
 import viper.silicon.logger.MemberSymbExLogger
 import viper.silicon.logger.SymbExLogger
-import viper.silicon.logger.writer.SymbExLogReportWriter
-import viper.silicon.logger.MemberSymbExLog
-import viper.silicon.logger.records.data.DeciderAssertRecord
+import viper.silicon.logger.records.data.CommentRecord
 import viper.silicon.logger.records.data.DeciderAssumeRecord
 import viper.silicon.logger.records.data.ProverAssertRecord
-import viper.silicon.logger.records.data.CommentRecord
-import viper.silicon.logger.records.structural.BranchingRecord
-import viper.silicon.logger.records.scoping.OpenScopeRecord
 import viper.silicon.logger.records.scoping.CloseScopeRecord
+import viper.silicon.logger.records.scoping.OpenScopeRecord
+import viper.silicon.logger.records.structural.BranchingRecord
+import viper.silicon.logger.writer.SymbExLogReportWriter
 //import viper.silicon.logger.LogConfig
 //import spray.json.JsArray
 
@@ -25,7 +24,7 @@ object SymbExLogBenchmarker {
     for (log <- l.logs) {
       val allRecords = SymbExLogReportWriter.getAllRecords(log.asInstanceOf[MemberSymbExLog].log)
       for (rec <- allRecords) rec match {
-        case _: DeciderAssertRecord | _: DeciderAssumeRecord | _:ProverAssertRecord | _:CommentRecord if !rec.isInstanceOf[CommentRecord] || rec.asInstanceOf[CommentRecord].toSimpleString == "push" || rec.asInstanceOf[CommentRecord].toSimpleString == "pop" =>
+        case _: DeciderAssumeRecord | _:ProverAssertRecord | _:CommentRecord if !rec.isInstanceOf[CommentRecord] || rec.asInstanceOf[CommentRecord].toSimpleString == "push" || rec.asInstanceOf[CommentRecord].toSimpleString == "pop" =>
           to_open = "prover"
         case _: BranchingRecord =>
           num_branches += 1
@@ -37,13 +36,14 @@ object SymbExLogBenchmarker {
           if (running_times contains s.refId) {
             val d: (String, Long, Long) = running_times(s.refId)
             accumulated_times(d._1) = (accumulated_times.getOrElse(d._1, 0): Long) - d._3 + d._2 + s.timeMs - d._2
+            running_times(s.refId) = (d._1, d._2, s.timeMs)
           }
         case _ =>
           to_open = ""
       }
     }
     println(s"Prover time: ${accumulated_times("prover")}")
+    println(s"Number of branches: ${num_branches}")
   }
-
 }
 
