1982e7358a3c29e36a270a0e88db080a4516aaa0
Dspil <dennisspiliopoylos@gmail.com>
2023-09-15 17:38:34 +0200

    Revert "benchmarking mods"
    This reverts commit 67fc9a33c535de7184c8a9255736202e8816ba75.

--- src/main/scala/rules/Brancher.scala
+++ src/main/scala/rules/Brancher.scala
@@ -56,13 +56,13 @@ object brancher extends BranchingRules {
     /* True if the then-branch is to be explored */
     val executeThenBranch = (
          skipPathFeasibilityCheck
-      ||{v.decider.prover.comment("branching!!!") ; !v.decider.check(negatedCondition, Verifier.config.checkTimeout())} || true)
+      || !v.decider.check(negatedCondition, Verifier.config.checkTimeout()))
 
     /* False if the then-branch is to be explored */
     val executeElseBranch = (
          !executeThenBranch /* Assumes that ast least one branch is feasible */
       || skipPathFeasibilityCheck
-      || {v.decider.prover.comment("branching!!!") ; !v.decider.check(condition, Verifier.config.checkTimeout())} || true)
+      || !v.decider.check(condition, Verifier.config.checkTimeout()))
 
     val parallelizeElseBranch = s.parallelizeBranches && !s.underJoin && executeThenBranch && executeElseBranch
 
