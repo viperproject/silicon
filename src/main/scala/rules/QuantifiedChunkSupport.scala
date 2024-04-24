8be227b5563fc70015a5714aa091dd68c4dd2812
Marco Eilers <eilers.marco@googlemail.com>
2024-04-24 15:42:37 +0200

    Undid accidental change

--- src/main/scala/rules/QuantifiedChunkSupport.scala
+++ src/main/scala/rules/QuantifiedChunkSupport.scala
@@ -1392,7 +1392,7 @@ object quantifiedChunkSupporter extends QuantifiedChunkSupport {
       // converting to a Z3 term.
       // During function verification, we should not define macros, since they could contain resullt, which is not
       // defined elsewhere.
-      val declareMacro = false && s.functionRecorder == NoopFunctionRecorder // && !Verifier.config.useFlyweight
+      val declareMacro = s.functionRecorder == NoopFunctionRecorder // && !Verifier.config.useFlyweight
 
       val permsProvided = ch.perm
       val permsTaken = if (declareMacro) {
