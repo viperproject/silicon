6a72ed1e3f18bd6ef363eb3091eb83cee378836d
Dspil <dennisspiliopoylos@gmail.com>
2025-09-04 13:31:56 +0200

    refactor: update cacheMapByName to store detailed core and branch hash info

--- src/main/scala/optimizations/Localization.scala
+++ src/main/scala/optimizations/Localization.scala
@@ -10,28 +10,31 @@ import viper.silicon.verifier.Verifier
 import scala.io.Source
 
 object ProofEssence {
-  private val cacheMapByName = scala.collection.mutable.Map.empty[String, Map[String, String]]
+  // methodName -> (branchHash -> core, supercore, set of branch hashes in the supercore)
+  private val cacheMapByName = scala.collection.mutable.Map.empty[String, (Map[String, String], String, Set[String])]
 
   val globalGuardName = "$GlobalGuard"
   val guardVariableName = "$LocalGuardVar"
 
   def branchGuards(name: String, branch: String): List[String] = {
     val coreCacheFile = new java.io.File(s"${Verifier.config.tempDirectory()}/${name}_unsatCoreCache.cache")
-    val cacheMap = cacheMapByName.getOrElseUpdate(name, {
+    val (coreMap, supercore, branchhashes) = cacheMapByName.getOrElseUpdate(name, {
       val source = Source.fromFile(coreCacheFile)
       try {
-        source.getLines().collect {
+        val coreMap: Map[String, String] = source.getLines().collect {
           case line if line.contains(":") =>
             val Array(hash, core) = line.split(":", 2)
             hash -> core
         }.toMap
+        (coreMap, "", Set(""))
       } finally source.close()
+      val supercore = coreMap.values.fold(Set[String].empty)((a, b) => a.split(";").toSet() | b.split(";").toSet())
+      val branchhashes = coreMap.keys.filter(hash => coreMap(hash).split(";").toSet()
     })
-    val cores = cacheMap.get(branch) match {
+    val cores = coreMap.get(branch) match {
       case Some(coresStr) => coresStr.split(";").toList
       case None => Nil
     }
-    println(s"Cores = $cores")
     cores
   }
 }
