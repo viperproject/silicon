a8e5d1d60bb6db8fe428f598e2fc7723d4d7c2ec
Marco Eilers <marcoeilers@users.noreply.github.com>
2025-11-26 14:01:46 +0100

    Making resource bounds the default instead of timeouts (#949)

--- src/main/scala/verifier/DefaultMainVerifier.scala
+++ src/main/scala/verifier/DefaultMainVerifier.scala
@@ -414,8 +414,8 @@ class DefaultMainVerifier(config: Config,
     sink.comment(s"\n; ${decider.prover.staticPreamble}")
     preambleReader.emitPreamble(decider.prover.staticPreamble, sink, true)
 
-    if (config.proverRandomizeSeeds) {
-      sink.comment(s"\n; Randomise seeds [--${config.rawProverRandomizeSeeds.name}]")
+    if (config.proverRandomizeSeeds()) {
+      sink.comment(s"\n; Randomise seeds [--${config.proverRandomizeSeeds.name}]")
       val options = decider.prover.randomizeSeedsOptions
         .map (key => s"(set-option :$key ${Random.nextInt(10000)})")
 
@@ -423,11 +423,11 @@ class DefaultMainVerifier(config: Config,
     }
 
     val smt2ConfigOptions =
-      config.proverConfigArgs.map { case (k, v) => s"(set-option :$k $v)" }
+      config.proverConfigArgs().map { case (k, v) => s"(set-option :$k $v)" }
 
     if (smt2ConfigOptions.nonEmpty) {
       // One can pass options to the prover. This allows to check whether they have been received.
-      val msg = s"Additional prover configuration options are '${config.proverConfigArgs.mkString(", ")}'"
+      val msg = s"Additional prover configuration options are '${config.proverConfigArgs().mkString(", ")}'"
       reporter report ConfigurationConfirmation(msg)
       logger info msg
       preambleReader.emitPreamble(smt2ConfigOptions, sink, true)
