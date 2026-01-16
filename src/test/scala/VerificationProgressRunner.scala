3d41f980a85d78002beb203fb21dec4ae3ce8a3b
Andrea Keusch <andrea.keusch@hotmail.com>
2026-01-16 09:58:28 +0100

    implement VerificationProgressRunner

---
+++ src/test/scala/VerificationProgressRunner.scala
@@ -0,0 +1,62 @@
+package viper.silicon.tests
+
+import viper.silicon.dependencyAnalysis.DependencyAnalysisReporter
+import viper.silver.ast.Program
+import viper.silver.frontend.SilFrontend
+import viper.silver.verifier.VerificationResult
+
+import java.io.PrintWriter
+import java.nio.file.{Files, Path, Paths}
+import scala.collection.convert.ImplicitConversions.`iterable AsScalaIterable`
+
+
+object VerificationProgressRunner extends DependencyAnalysisTestFramework {
+
+  val ignores: Seq[String] = Seq.empty
+  val pathToTests: String = "src/test/resources/"
+  val testDirectories: Seq[String] = Seq(
+    "dependencyAnalysisTests/verificationProgress/incrRand"
+  )
+
+  def main(args: Array[String]): Unit = {
+    testDirectories foreach computeProgressForDir
+  }
+
+  def computeProgressForDir(dirName: String): Unit = {
+    val writer = new PrintWriter(pathToTests + dirName + "/results_" + System.currentTimeMillis() + ".csv")
+
+    val directoryStream = Files.newDirectoryStream(Paths.get(pathToTests, dirName))
+    val dirContent = directoryStream.toList
+    writer.println("File name,Progress (Peter),Progress (Lea)")
+    println("File name,Progress (Peter),Progress (Lea)")
+
+    for (filePath: Path <- dirContent.sorted
+         if Files.isReadable(filePath)) {
+      val rawFileName = filePath.getFileName.toString
+      if (rawFileName.endsWith(".vpr")) {
+        val vprFileName = rawFileName.replace(".vpr", "")
+        if (!ignores.contains(vprFileName))
+          resetFrontend()
+        computeProgress(dirName + "/", vprFileName, frontend, writer)
+      }
+    }
+    writer.close()
+  }
+
+
+  def computeProgress(filePrefix: String,
+                      fileName: String,
+                      frontend: SilFrontend,
+                      writer: PrintWriter): Unit = {
+
+    val program: Program = tests.loadProgram(filePrefix, fileName, frontend)
+    val _: VerificationResult = frontend.verifier.verify(program)
+
+    val joinedDependencyGraphInterpreter = frontend.reporter.asInstanceOf[DependencyAnalysisReporter].joinedDependencyGraphInterpreter
+
+    val (progressPeter, progressLea, _) = joinedDependencyGraphInterpreter.get.computeVerificationProgress()
+
+    writer.println(s"$fileName,\t$progressPeter,\t$progressLea")
+    println(s"$fileName,\t$progressPeter,\t$progressLea")
+  }
+}
